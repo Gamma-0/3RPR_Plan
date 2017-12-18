2a07c036b58221cd2fd3d8541f46e76fc8c98f82
Margaux Seoane <naoru@Exagames.home>
2017-12-18 18:53:59 +0100

    Correction orthographe

--- MGI.m
+++ MGI.m
@@ -25,8 +25,8 @@ A1 = [0 0];   % A1 se situe toujours à l'origine
 A2 = [30 0];  % A2 a pour ordonné 0
 A3 = [15 30];
 
-% Longueur des cotés du triangle (le triangle doit etre equilateral)
-l = 3; 
+% Longueur des cotés du triangle (le triangle est equilateral)
+l = 3;
 
 % Coordonnées du centre de gravité du triangle R1R2R3 représentant le robot.
 G = [15 ; 10];
@@ -35,13 +35,13 @@ theta = 0;
 
 RotZ = [cos(theta) -sin(theta);
         sin(theta) cos(theta)];
-    
+
 %--------------------------------------------------------------------------
 %--------------------------- Calcul du MGI --------------------------------
 %--------------------------------------------------------------------------
 
 % Définir les coordonnées de G dans un repère dont l'origine est G,
-% dont l'axe des abscisses est parallèle à R1R2, 
+% dont l'axe des abscisses est parallèle à R1R2,
 % et l'axe des ordonnées passe par le point R3.
 
 R1_G = [-l/2 ; -l/3];                % Déterminer point R1 dans le repère défini depuis G
@@ -49,36 +49,36 @@ R1 = G + RotZ * R1_G;                 % Coordonnées du point depuis le repère 
 
 l1 = euclidean_distance(A1, R1); % Distance A1R1
 
-R2_G = [l/2 ; -l/3];                % Déterminer point R2 dans le repère défini depuis G 
+R2_G = [l/2 ; -l/3];                % Déterminer point R2 dans le repère défini depuis G
 R2 = G + RotZ * R2_G;                % Coordonnées du point depuis le repère défini en A2
 
 l2 = euclidean_distance(A2, R2); % Distance A2R2
 
-R3_G = [0 ; 2*l/3];               % Déterminer point R3 dans le repère défini depuis G 
+R3_G = [0 ; 2*l/3];               % Déterminer point R3 dans le repère défini depuis G
 R3 = G + RotZ * R3_G;                % Coordonnées du point depuis le repère défini en A3
 
 l3 = euclidean_distance(A3, R3); % Distance A3R3
 
 P = [l1,l2,l3]; % Solution du MGI (longueur des articulations)
 % Chaque colonne de cette matrice détermine une longueur d'articulation.
-% l1 : longueur de l'articulation A1R1. 
-% l2 : longueur de l'articulation A2R2. 
-% l3 : longueur de l'articulation A3R3. 
+% l1 : longueur de l'articulation A1R1.
+% l2 : longueur de l'articulation A2R2.
+% l3 : longueur de l'articulation A3R3.
 
 a1 = atanoa(R1,A1);
 a2 = atanoa(R2,A2);
 a3 = atanoa(A3,R3);
 At1 = [a1, a2, a3] ; % Solution du MGI (angle de la rotation de la base des membres)
 % Chaque colonne de cette matrice détermine l'angle de la première rotation de chaque membre
-% a1 : angle de l'axe x à A1R1. 
-% a2 : angle de l'axe x à A2R2. 
-% a3 : angle de l'axe -x à A3R3. 
+% a1 : angle de l'axe x à A1R1.
+% a2 : angle de l'axe x à A2R2.
+% a3 : angle de l'axe -x à A3R3.
 
 a1 = angle_two_lines(A1, R1, R1, G);
 a2 = angle_two_lines(A2, R2, R2, G);
-a3 = angle_two_lines(A3, R3, R3, G); 
+a3 = angle_two_lines(A3, R3, R3, G);
 At2 = [a1, a2, a3]; % Solution du MGI (angle de la rotation au niveau du corps de robot)
-% Chaque colonne de cette matrice détermine l'angle de la deuxième rotation de chaque membre 
+% Chaque colonne de cette matrice détermine l'angle de la deuxième rotation de chaque membre
 % a1 : angle de A1R1 à R1G.
 % a2 : angle de A2R2 à R2G.
 % a3 : angle de A3R3 à R3G.
--- README.md
+++ README.md
@@ -7,18 +7,18 @@ Il y a deux fichiers principaux :
 
 Ces deux fichiers utilisent des fonctions, dont les fichiers sont situés dans le dossier `functions`. Il est important de bien rajouter ce répertoire dans le path de matlab pour que les fonctions puissent être utilisées. Il est aussi possible de mettre les fonctions dans le même répertoire que les programmes principaux.
 
-Avant d'exécuter MGI.m ou MGD.m, il est possible de paramétrer des variables en début de fichier, dans la partie `Variables à paramétrer`, afin de calculer le MGI ou le MGD d'une configuration particulière. La partie `Calcul` ne doit pas être modifié.
+Avant d'exécuter MGI.m ou MGD.m, il est possible de paramétrer des variables en début de fichier, dans la partie `Variables à paramétrer`, afin de calculer le MGI ou le MGD d'une configuration particulière. La partie `Calcul` ne doit pas être modifiée.
 
 ## MGI
 Limite : le MGI ne peut être calculé que pour des triangles équilatéraux.  
 La solution du système est répartie dans trois variables :
-- At1 pour la première liaison rotoide,
+- At1 pour la première liaison rotoïde,
 - P pour la liaison prismatique,
-- At2 Pour la deuxième liaison rotoide.
+- At2 pour la deuxième liaison rotoïde.
 
 ## MGD
 Le MGD peut être calculé pour tout type de triangle.  
-Les solutions du système sont dans la variable G  et sont au nombre de 6 :
+Les solutions du système sont dans la variable G, chaque ligne de G représente une solution et il peut y en avoir jusqu'à 6 :
 - Colonne 1 : coordonnée en abscisse du centre de gravité
 - Colonne 2 : coordonnée en ordonnée du centre de gravité
 - Colonne 3 : rotation sur l'axe z du triangle
